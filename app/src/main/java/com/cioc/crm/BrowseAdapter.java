bc9cadcf1f5964326f6c4cd99fad14279988c1a4
Your Name <you@example.com>
2018-03-08 15:01:19 +0530

    additional code

--- app/src/main/java/com/cioc/crm/BrowseAdapter.java
+++ app/src/main/java/com/cioc/crm/BrowseAdapter.java
@@ -8,12 +8,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Ashish on 3/7/2018.
  */
 
 public class BrowseAdapter extends RecyclerView.Adapter<BrowseAdapter.MyHolder> {
+
+    public static int contact_images[] = {R.drawable.male, R.drawable.woman, R.drawable.male, R.drawable.woman, R.drawable.male,
+            R.drawable.woman,R.drawable.male, R.drawable.woman, R.drawable.male, R.drawable.woman};
+    public static String contact_names[] = {"Samuel D. Pollock", "Rita Stith", "Ronald Allen", "Veronica Woods", "Robert Y. Griffin","Purity Supreme", "P. Madhawa", "Shweta Stith",
+            "Robert H. Peebles","Lorie T. Morales"};
+    public static String contact_companies[] = {"First Choice Yard Help", "Foxmoor", "First Choice Yard Help ", "Foxmoor", "First Choice Yard Help ","Purity Supreme", "Pomeroy's",
+            "First Choice Yard Help ","Purity Supreme","Pomeroy's"};
+    public static String contact_designations[] = {"Information systems manager", "Sales Manager", "General Manager", "Sales Manager", "Information systems manager","Sales Manager",
+            "Sales executive","Sales Manager","Sales executive","Sales Manager"};
+    public static String contact_cnos[] = {"7854211505", "8542120007", "8542450070", "854214512", "8542191345","95854218454", "8458542121","788542145","7215485421","8285421145"};
+    public static String contact_emails[] = {"SamuelDPollock@teleworm.us", "RitaOStith@jourrapide.com", "RonaldSAllen@jourrapide.com", "VeronicaJWoods@armyspy.com", "JamesMPacheco@teleworm.us",
+            "PPMadhawa@jourrapide.com", "RobertYGriffin@teleworm.us","ShwetaOStith@jourrapide.com","RobertHPeebles@armyspy.com","LorieTMorales@jourrapide.com"};
+
     Context context;
 
     public BrowseAdapter(Context context){
@@ -31,27 +45,42 @@ public class BrowseAdapter extends RecyclerView.Adapter<BrowseAdapter.MyHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        holder.browseImage.setImageResource(contact_images[position]);
+        holder.browseName.setText(contact_names[position]);
+        holder.browseDesignation.setText(contact_designations[position]);
+        holder.browseCompany.setText(contact_companies[position]);
+        holder.browseMob.setText(contact_cnos[position]);
+        holder.browseEmail.setText(contact_emails[position]);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contact_names.length;
     }
 
     public class MyHolder extends RecyclerView.ViewHolder {
 
-        ImageView browse_image;
-        TextView browse_name,browse_designation, browse_company, browse_phone, browse_email;
+        ImageView browseImage, editProfile, viewDetails;
+        TextView browseName,browseDesignation, browseCompany, browseMob, browseEmail;
 
         public MyHolder(View itemView) {
             super(itemView);
-            browse_image = itemView.findViewById(R.id.contacts_image_browse);
-            browse_name = itemView.findViewById(R.id.contacts_name_browse);
-            browse_designation = itemView.findViewById(R.id.contacts_designation_browse);
-            browse_company = itemView.findViewById(R.id.contacts_company_browse);
-            browse_phone = itemView.findViewById(R.id.contacts_no_browse);
-            browse_email = itemView.findViewById(R.id.contacts_email_browse);
+            browseImage = itemView.findViewById(R.id.contacts_image_browse);
+            browseName = itemView.findViewById(R.id.contacts_name_browse);
+            browseDesignation = itemView.findViewById(R.id.contacts_designation_browse);
+            browseCompany = itemView.findViewById(R.id.contacts_company_browse);
+            browseMob = itemView.findViewById(R.id.contacts_no_browse);
+            editProfile = itemView.findViewById(R.id.edit_profile);
+            browseEmail = itemView.findViewById(R.id.contacts_email_browse);
+            viewDetails = itemView.findViewById(R.id.view_details);
+
+            editProfile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, ""+getLayoutPosition(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/cioc/crm/NewContactFragment.java
+++ app/src/main/java/com/cioc/crm/NewContactFragment.java
@@ -1,17 +1,30 @@
 package com.cioc.crm;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewContactFragment extends Fragment {
+
+    EditText new_full_name, new_email, new_mob_no, new_company, new_email_duplicate, new_mob_no_duplicate, new_designation, new_notes, new_linkedin, new_fb;
+    TextView new_dp, new_dp_attach;
+    Button saveNewContact;
 
 
     public NewContactFragment() {
@@ -23,7 +36,51 @@ public class NewContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_contact, container, false);
+        View v = inflater.inflate(R.layout.fragment_new_contact, container, false);
+
+        new_full_name = v.findViewById(R.id.contacts_full_name);
+        new_email = v.findViewById(R.id.contacts_email);
+        new_mob_no = v.findViewById(R.id.contacts_mobile);
+        new_company = v.findViewById(R.id.contacts_company);
+        new_email_duplicate = v.findViewById(R.id.contacts_email_secondary);
+        new_mob_no_duplicate = v.findViewById(R.id.contacts_mobile_secondary);
+        new_designation = v.findViewById(R.id.contacts_designation);
+        new_notes = v.findViewById(R.id.contacts_notes);
+        new_linkedin = v.findViewById(R.id.contacts_linkedin);
+        new_fb = v.findViewById(R.id.contacts_facebook);
+        new_dp = v.findViewById(R.id.contact_profile_photo);
+        new_dp_attach = v.findViewById(R.id.dp_attached);
+        saveNewContact = v.findViewById(R.id.save_newContacts);
+
+        new_dp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), 111);
+            }
+        });
+
+
+
+        return v;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 111){
+            if (resultCode == getActivity().RESULT_OK){
+                Uri uri = data.getData();
+                try {
+                    Bitmap bit = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),uri);
+                    new_dp_attach.setVisibility(View.VISIBLE);
+                    new_dp_attach.setText("Attached");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
