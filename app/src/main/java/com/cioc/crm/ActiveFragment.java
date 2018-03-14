a84929776cb677d84498612442a61f24730de5d2
Your Name <you@example.com>
2018-03-14 16:19:14 +0530

    some modification

--- app/src/main/java/com/cioc/crm/ActiveFragment.java
+++ app/src/main/java/com/cioc/crm/ActiveFragment.java
@@ -23,7 +23,9 @@ public class ActiveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_active, container, false);
+        View v = inflater.inflate(R.layout.fragment_active, container, false);
+
+        return v;
     }
 
 }
